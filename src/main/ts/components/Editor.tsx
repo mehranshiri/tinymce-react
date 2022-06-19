46718d46a3377cfc743dd4314b649037374e6164
mehr4nshiri <mehr4nshiri@gmail.com>
2022-06-19 18:45:07 +0430

    change: change js files direction

--- src/main/ts/components/Editor.tsx
+++ src/main/ts/components/Editor.tsx
@@ -180,7 +180,8 @@ export class Editor extends React.Component<IAllProps> {
     } else {
       const channel = this.props.cloudChannel;
       const apiKey = this.props.apiKey ? this.props.apiKey : 'no-api-key';
-      return `https://cdn.tiny.cloud/1/${apiKey}/tinymce/${channel}/tinymce.min.js`;
+      // return `https://cdn.tiny.cloud/1/${apiKey}/tinymce/${channel}/tinymce.min.js`;
+      return `/src/assets/js/tinymce-${channel}.min.js`;
     }
   }
 
